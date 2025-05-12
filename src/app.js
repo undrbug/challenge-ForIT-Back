1c92aa5ace8276f73e1aa10a1a07cf1b4d1df884
hGauna <undrbug@gmail.com>
2025-05-12 18:37:40 -0300

    inicializacion y estructura basica del servidor

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors'
+import dotenv from 'dotenv'
+import routerToDos from './src/routes/todo.routes.js'
+
+//instanciamos express
+const app = express();
+
+dotenv.config();
+
+app.use(express.urlencoded({ extended: false }));
+
+//establecemos el puerto de escucha
+const PORT = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
+
+
+//establecemos las rutas
+app.use('/api', routerToDos)
+app.use((req, res, next) => {
+    res.send("eeeeerrrrr 404")
+})
+
+//iniciamos el servidor
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
